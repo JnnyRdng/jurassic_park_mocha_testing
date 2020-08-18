9e3555ec36818be9a77be636c689b0d43d392115
Jonny Reading <jonnyreading@gmail.com>
2020-08-18 17:03:12 +0100

    can remove a dinosaur from the park

--- models/park.js
+++ models/park.js
@@ -8,4 +8,14 @@ Park.prototype.addDinosaur = function (dinosaur) {
     this.dinosaurs.push(dinosaur);
 }
 
+Park.prototype.removeDinosaur = function (dinosaur) {
+    let dinosaursToKeep = [];
+    for (let dino of this.dinosaurs) {
+        if (dino !== dinosaur) {
+            dinosaursToKeep.push(dino);
+        }
+    }
+    this.dinosaurs = dinosaursToKeep;
+}
+
 module.exports = Park;
